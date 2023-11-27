0f8f55026ad92e2c96bfd1fc2f81cf81b8ad7a4b
jossharlequin <146377666+jossharlequin@users.noreply.github.com>
2023-11-26 21:39:23 -0700

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e5fa55723e53863c566bde0b213b04ac"
+weather_api_key = "YOUR CODE HERE"
 
 # Geoapify API Key
-geoapify_key = "d2380e5596ad4b15867d1b9339a61eb5"
+geoapify_key = "YOUR CODE HERE"
